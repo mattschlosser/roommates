896679b90ebecf4f9f7a1ff000376bb912031f2e
Matt Schlosser <websitemakingguy@gmail.com>
2019-01-19 13:09:21 -0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Roommates</h1>
+          <div className="loginBox">
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="password"/>
+            <button>Submit</button>
+          </div>
+          <hr/>
+          <button>Sign Uo</button>
         </header>
       </div>
     );
